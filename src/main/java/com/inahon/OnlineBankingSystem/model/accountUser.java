ea1336e91cbef1cb6e50a01d26f33c29c1a419aa
elie <inahon.elie17@yahoo.com>
2025-03-08 02:35:51 -0500

    Created model package and implemented User entity class.

---
+++ src/main/java/com/inahon/OnlineBankingSystem/model/accountUser.java
@@ -0,0 +1,23 @@
+package com.inahon.OnlineBankingSystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class accountUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+}
