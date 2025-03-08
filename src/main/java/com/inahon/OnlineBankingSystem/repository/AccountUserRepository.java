c3110c79b6c6820bb4ea9d840d08d38525579cc3
elie <inahon.elie17@yahoo.com>
2025-03-08 11:31:33 -0500

    Add Repository Interfaces for BankAccount.

--- src/main/java/com/inahon/OnlineBankingSystem/repository/AccountUserRepository.java
+++ src/main/java/com/inahon/OnlineBankingSystem/repository/AccountUserRepository.java
@@ -2,7 +2,9 @@ package com.inahon.OnlineBankingSystem.repository;
 
 import com.inahon.OnlineBankingSystem.model.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountUserRepository extends JpaRepository<AccountUser, Long> {
     AccountUser findByEmail(String email);
 }
