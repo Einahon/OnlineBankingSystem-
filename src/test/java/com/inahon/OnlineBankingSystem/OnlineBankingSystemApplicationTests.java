81b3e0e3eeca18b9f47b869c03545905d0e08c54
elie <inahon.elie17@yahoo.com>
2025-03-08 01:43:25 -0500

    initial commit

---
+++ src/test/java/com/inahon/OnlineBankingSystem/OnlineBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inahon.OnlineBankingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
